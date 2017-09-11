1ca642a7c2ff24c9d45752e5b454e69f13d2c802
Daniel Constantin <hello@danielconstantin.net>
2017-09-11 22:12:35 +0200

    Add firebase db

---
+++ src/fire.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyCsWZGwGZHAzc7RtQYRKUatVE4NtMxFMO8",
+  authDomain: "amber-dcnet.firebaseapp.com",
+  databaseURL: "https://amber-dcnet.firebaseio.com",
+  projectId: "amber-dcnet",
+  storageBucket: "amber-dcnet.appspot.com",
+  messagingSenderId: "214047550254"
+});
